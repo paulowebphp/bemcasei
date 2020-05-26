7c85d6c7b62179079ce30a8650e6fa11b6f2e43f
paulowebphp <paulowebphp@gmail.com>
2020-05-26 14:30:21 -0300

    Mudando o nome do Ebook

--- res/js/main2.js
+++ res/js/main2.js
@@ -59,8 +59,8 @@ $("#ebook-download").on("click", function(e){
     e.preventDefault();
 
     let link = document.createElement('a');
-	link.href = '/ebook/A_Maneira_Mais_Segura_e_Simples_De_Realizar_Seu_Casamento_Durante_A_Pandemia.pdf';
-	link.download = 'A_Maneira_Mais_Segura_e_Simples_De_Realizar_Seu_Casamento_Durante_A_Pandemia.pdf';
+	link.href = '/ebook/A_Maneira_Mais_Segura_e_Simples_De_Realizar_Seu_Casamento.pdf';
+	link.download = 'A_Maneira_Mais_Segura_e_Simples_De_Realizar_Seu_Casamento.pdf';
 	link.click();
 	
 
