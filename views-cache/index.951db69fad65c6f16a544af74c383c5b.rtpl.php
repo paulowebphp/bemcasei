75258ab0d4a374639a99c5f3ebda651c361c5a2d
paulowebphp <paulowebphp@gmail.com>
2020-08-14 10:08:54 -0300

    Aumentando tamanho do da imagem do Ebook e trocando a Call To Action para mais específico

--- views-cache/index.951db69fad65c6f16a544af74c383c5b.rtpl.php
+++ views-cache/index.951db69fad65c6f16a544af74c383c5b.rtpl.php
@@ -70,7 +70,7 @@
 
 
 
-                      <button type="submit" class="button7">Quero Receber Meu E-book!</button>
+                      <button type="submit" class="button7">Quero Realizar Meu Casamento Com Segurança na Quarentena!</button>
 
                     </form>
 
